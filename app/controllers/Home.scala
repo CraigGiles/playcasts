90909bf2772d270d28331873aea12d92cfc99e5a
Craig Giles <craig@gilesc.com>
2014-07-06 13:27:20 -0700

    added about and contacts

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -6,7 +6,14 @@ import play.api.mvc._
 object Home extends Controller {
 
     def index = Action {
-        Ok(views.html.home())
+        Ok(views.html.home.index())
     }
 
+    def about = Action {
+        Ok(views.html.home.about())
+    }
+
+    def contact = Action {
+        Ok(views.html.home.contact())
+    }
 }
