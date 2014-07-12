f2222554843133951c81326b23a3e60869e13fc1
Craig Giles <craig@gilesc.com>
2014-07-11 21:56:06 -0700

    Added user class and various anorm extensions
    Anorm extensions will convert MySQL TIMESTAMP to DateTime objects

---
+++ app/models/User.scala
@@ -0,0 +1,28 @@
+package models
+
+import org.joda.time._
+import org.joda.time.format._
+
+import play.api.db.DB
+import play.api.Play.current
+
+import anorm._
+import anorm.SqlParser._
+import java.sql._
+import utilities.AnormExtension._
+
+case class User(id: Long, name: String, email: String, password_hash: String, 
+    created_at: DateTime, updated_at: DateTime, deleted_at: DateTime)
+
+object User {
+    def findAll: Seq[User] = DB.withConnection { 
+        implicit connection =>
+        SQL("select * from users").map( row =>
+            User(row[Long]("id"), row[String]("name"), row[String]("email"), 
+                 row[String]("password_hash"), row[DateTime]("created_at"), 
+                 row[DateTime]("updated_at"), row[DateTime]("deleted_at"))
+        ).list
+    }
+}
+
+//param: Option[String] = None
