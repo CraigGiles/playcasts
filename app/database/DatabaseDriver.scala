84306064e8dbcd5573370072df329520a9cb1eb9
Craig Giles <craig@gilesc.com>
2015-02-08 11:40:21 -0800

    updated database driver to include play type

--- app/database/DatabaseDriver.scala
+++ app/database/DatabaseDriver.scala
@@ -1,12 +1,14 @@
 package database
 
+import play.api.Play
+
 import scala.slick.driver.{H2Driver, JdbcProfile, MySQLDriver, PostgresDriver}
 
 object DatabaseDriver {
   val simple = profile.simple
 
   lazy val profile: JdbcProfile = {
-    sys.env.get("db.default.type") match {
+    Play.current.configuration.getString("db.default.type") match {
       case Some("mysql")    => MySQLDriver
       case Some("postgres") => PostgresDriver
       case _                => H2Driver
