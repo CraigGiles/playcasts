c4ed178ace5bcac1a741bf55bad8aa59e7cb7768
Craig Giles <craig@gilesc.com>
2015-02-14 13:23:20 -0800

    users are now able to get added to the system

--- app/database/slick/UserRepositoryComponentSlick.scala
+++ app/database/slick/UserRepositoryComponentSlick.scala
@@ -10,21 +10,27 @@ trait UserRepositoryComponentSlick extends UserRepositoryComponent {
   def userRepository = new SlickUserRepository
 
   class SlickUserRepository extends UserRepository with Slickable {
-    lazy val users = TableQuery[Users]
+    lazy val usersTbl = TableQuery[Users]
 
     override def findById(id: Int): Option[User] = {
       database withSession { implicit session =>
-        val user = users.list.find(u => u._1 == Some(id))
-        user.map(u => Users.create(u))
+        val byId = usersTbl.findBy(_.id)
+        byId(Some(id)).list.headOption
       }
     }
 
     override def save(user: User): Unit = {
-
+      database withSession { implicit session =>
+        usersTbl.insert(user)
+      }
     }
 
     override def findByEmail(email: String): Option[User] = {
-      None
+      database withSession { implicit session =>
+        val byId = usersTbl.findBy(_.email)
+        byId(email).list.headOption
+      }
     }
+
   }
 }
--- app/database/slick/tables/Users.scala
+++ app/database/slick/tables/Users.scala
@@ -4,8 +4,9 @@ import org.joda.time.DateTime
 import database.DatabaseDriver.simple._
 import scala.slick.lifted.ProvenShape
 import com.github.tototoshi.slick.MySQLJodaSupport._
+import models.User
 
-class Users(tag: Tag) extends Table[(Option[Int], String, String, String, DateTime, DateTime, Option[DateTime])](tag, "users") {
+class Users(tag: Tag) extends Table[User](tag, "users") {
   def id: Column[Option[Int]] = column[Option[Int]]("id", O.PrimaryKey, O.AutoInc)
   def name: Column[String] = column[String]("name")
   def email: Column[String] = column[String]("email")
@@ -14,8 +15,7 @@ class Users(tag: Tag) extends Table[(Option[Int], String, String, String, DateTi
   def updated_at: Column[DateTime] = column[DateTime]("updated_at")
   def deleted_at: Column[Option[DateTime]] = column[Option[DateTime]]("deleted_at", O.Nullable)
 
-  def * : ProvenShape[(Option[Int], String, String, String, DateTime, DateTime, Option[DateTime])] =
-    (id, name, email, password, created_at, updated_at, deleted_at)
+  def * = (id, name, email, password, created_at, updated_at, deleted_at) <> ((User.apply _).tupled, User.unapply)
 }
 
 
